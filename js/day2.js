57e7efb67f65a3b89597e5e94039c6fadd8d5b84
Miles Angels <milesmarieangels@gmail.com>
2022-02-28 19:10:12 -0800

    practice for day 2

--- js/day2.js
+++ js/day2.js
@@ -3,8 +3,8 @@ let num = 99;
 let bigInt = 13842467573012736502936473523917416569692728726198201937109373629136198362746n;
 
 console.log(longWord.length);
-//typeof longWord;
+console.log(typeof longWord);
 console.log(num);
-//typeof num;
+console.log(typeof num);
 console.log(bigInt);
-//typeof bigInt;
+console.log(typeof bigInt);
