aaeb91ec2e5436cf2ab4e061f51889ca8b529ef6
Miles Angels <milesmarieangels@gmail.com>
2022-02-28 18:42:18 -0800

    Day 2: data types

---
+++ day2/day2.js
@@ -0,0 +1,10 @@
+let longWord = "Supercalifragilisticexpialidocious";
+let num = 99;
+let bigInt = 13842467573012736502936473523917416569692728726198201937109373629136198362746n;
+
+console.log(longWord.length);
+//typeof longWord;
+console.log(num);
+//typeof num;
+console.log(bigInt);
+//typeof bigInt;
